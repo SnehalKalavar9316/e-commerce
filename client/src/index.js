13100b8e8b7582c06a8e530ad1f2c5d5659b9e88
SnehalKalavar9316 <snehalkalwar@gmail.com>
2025-01-15 15:57:25 +0530

    Route setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {Camera as CameraIcon} from "lucide-react"
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import Home from "./Views/Home";
+import Signup from "./Views/Signup";
+import Login from "./Views/Login";
+import NotFound from './Views/404';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Home/>,
+  },
+  {
+    path:"/signup",
+    element:<Signup/>,
+  },
+  {
+    path:"/login",
+    element:<Login/>,
+  },
+  {
+    path:"*",
+    element:<NotFound/>,
+  },
+])
 root.render(
-  <>
-  <CameraIcon/>
-  <h1 className='text-red-500'>E-Commerce</h1>
-  </>
+  <RouterProvider router={router}/>
+ 
+ 
   
 );
 
