8e8a771e9d54d0a976fd012bcad973d79d61e281
SnehalKalavar9316 <snehalkalwar@gmail.com>
2025-01-15 15:45:23 +0530

    Icon setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import {Camera as CameraIcon} from "lucide-react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-  
+  <CameraIcon/>
   <h1 className='text-red-500'>E-Commerce</h1>
   </>
   
